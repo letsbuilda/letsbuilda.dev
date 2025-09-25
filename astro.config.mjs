1dac6113aeb283d642c175bf3315b1a01bd9994b
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2025-09-25 04:12:16 +0000

    Re-init Astro with Auth0
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [react()],
+});

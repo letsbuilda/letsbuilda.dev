a3e2158c62aa49a2fca54240982be4bd3191d20e
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2026-04-14 18:18:27 +0000

    Run lints
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
